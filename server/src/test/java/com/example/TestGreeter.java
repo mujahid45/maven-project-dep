d7b8a4788154d2c3ec3931ce32bcd06a27ee203a
mujahid45 <mujahid.dsi@gmail.com>
2018-07-31 10:05:52 +0530

    Update TestGreeter.java
    fsd

--- server/src/test/java/com/example/TestGreeter.java
+++ server/src/test/java/com/example/TestGreeter.java
@@ -19,7 +19,7 @@ public class TestGreeter {
 
   @Test
   public void greetShouldIncludeTheOneBeingGreeted() {
-    String someone = "World";
+    String someone = "Virtusa";
 
     assertThat(greeter.greet(someone), containsString(someone));
   }
