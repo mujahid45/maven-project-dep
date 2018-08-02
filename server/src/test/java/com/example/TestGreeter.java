480c013054dac5efeeca84093c4c814ea697b422
mujahid45 <mujahid.dsi@gmail.com>
2018-08-02 15:13:44 +0530

    Update TestGreeter.java
    sdg

--- server/src/test/java/com/example/TestGreeter.java
+++ server/src/test/java/com/example/TestGreeter.java
@@ -8,7 +8,7 @@ import static org.hamcrest.Matchers.greaterThan;
 import static org.junit.Assert.assertThat;
 import static org.junit.matchers.JUnitMatchers.containsString;
 
-public class TestGreeter {
+private class TestGreeter {
 
   private Greeter greeter;
 
