39a73082648afc3913bff0fafd85521825f8ab39
mujahid45 <mujahid.dsi@gmail.com>
2018-08-02 15:07:50 +0530

    Update TestGreeter.java
    deleted

--- server/src/test/java/com/example/TestGreeter.java
+++ server/src/test/java/com/example/TestGreeter.java
@@ -26,7 +26,7 @@ public class TestGreeter {
 
   @Test
   public void greetShouldIncludeGreetingPhrase() {
-    String someone = "World";
+     someone = "World";
 
     assertThat(greeter.greet(someone).length(), is(greaterThan(someone.length())));
   }
