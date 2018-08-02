fdf4dc702bb1aae356d32893e31ec95e24a47ce1
mujahid45 <mujahid.dsi@gmail.com>
2018-08-02 15:12:41 +0530

    Update TestGreeter.java
    fd

--- server/src/test/java/com/example/TestGreeter.java
+++ server/src/test/java/com/example/TestGreeter.java
@@ -26,7 +26,7 @@ public class TestGreeter {
 
   @Test
   public void greetShouldIncludeGreetingPhrase() {
-     String someone = "WORLD";
+     String someone = "World";
 
     assertThat(greeter.greet(someone).length(), is(greaterThan(someone.length())));
   }
